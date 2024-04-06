561007370373aeacd3893c0937bc2ecf0f9d681e
Antônio Guedes <146470610+tonigds1@users.noreply.github.com>
2024-04-06 13:57:52 +0000

    Atualização da atividade

--- atv1/src/pages/Home.tsx
+++ atv1/src/pages/Home.tsx
@@ -14,58 +14,58 @@ export const Home = () => {
                 </tr>
                 <tr>
                     <td><img src="https://brasil.un.org/profiles/undg_country/themes/custom/undg/images/SDGs/pt-br/SDG-2.svg"></img></td>
-                    <td>Fome Zero e Agricultura Sustentável</td>
-                    <td>O objetivo é acabar com a fome, alcançar a segurança alimentar, melhorar a nutrição e promover a agricultura sustentável, garantindo o acesso de todos a alimentos seguros, nutritivos e suficientes durante todo o ano.</td>
+                    <td><h2>Fome Zero e Agricultura Sustentável</h2></td>
+                    <td><p>O objetivo é acabar com a fome, alcançar a segurança alimentar, melhorar a nutrição e promover a agricultura sustentável, garantindo o acesso de todos a alimentos seguros, nutritivos e suficientes durante todo o ano.</p></td>
                 </tr>
                 <tr>
                     <td><img src="https://brasil.un.org/profiles/undg_country/themes/custom/undg/images/SDGs/pt-br/SDG-3.svg"></img></td>
-                    <td>Saúde e Bem-Estar</td>
-                    <td>Este objetivo visa garantir uma vida saudável e promover o bem-estar para todos, em todas as idades, abordando questões como saúde materna e infantil, prevenção de doenças, acesso a serviços de saúde de qualidade e promoção do bem-estar mental.</td>
+                    <td><h2>Saúde e Bem-Estar</h2></td>
+                    <td><p>Este objetivo visa garantir uma vida saudável e promover o bem-estar para todos, em todas as idades, abordando questões como saúde materna e infantil, prevenção de doenças, acesso a serviços de saúde de qualidade e promoção do bem-estar mental.</p></td>
                 </tr>
                 <tr>
                     <td><img src="https://brasil.un.org/profiles/undg_country/themes/custom/undg/images/SDGs/pt-br/SDG-4.svg"></img></td>
-                    <td>Educação de Qualidade</td>
-                    <td>O objetivo é garantir uma educação inclusiva, equitativa e de qualidade, promovendo oportunidades de aprendizagem ao longo da vida para todos, independentemente de gênero, localização geográfica ou circunstâncias socioeconômicas.</td>
+                    <td><h2>Educação de Qualidade</h2></td>
+                    <td><p>O objetivo é garantir uma educação inclusiva, equitativa e de qualidade, promovendo oportunidades de aprendizagem ao longo da vida para todos, independentemente de gênero, localização geográfica ou circunstâncias socioeconômicas.</p></td>
                 </tr>
                 <tr>
                     <td><img src="https://brasil.un.org/profiles/undg_country/themes/custom/undg/images/SDGs/pt-br/SDG-5.svg"></img></td>
-                    <td>Igualdade de Gênero</td>
-                    <td>Este objetivo busca alcançar a igualdade de gênero e capacitar todas as mulheres e meninas, promovendo a igualdade de oportunidades em áreas como educação, emprego, participação política e tomada de decisões em todos os níveis.</td>
+                    <td><h2>Igualdade de Gênero</h2></td>
+                    <td><p>Este objetivo busca alcançar a igualdade de gênero e capacitar todas as mulheres e meninas, promovendo a igualdade de oportunidades em áreas como educação, emprego, participação política e tomada de decisões em todos os níveis.</p></td>
                 </tr>
                 <tr>
                     <td><img src="https://brasil.un.org/profiles/undg_country/themes/custom/undg/images/SDGs/pt-br/SDG-6.svg"></img></td>
-                    <td>Água Limpa e Saneamento</td>
-                    <td>O objetivo é garantir o acesso universal a água potável segura e saneamento adequado, além de promover práticas sustentáveis de gestão de recursos hídricos para todos.</td>
+                    <td><h2>Água Limpa e Saneamento</h2></td>
+                    <td><p>O objetivo é garantir o acesso universal a água potável segura e saneamento adequado, além de promover práticas sustentáveis de gestão de recursos hídricos para todos.</p></td>
                 </tr>
                 <tr>
                     <td><img src="https://brasil.un.org/profiles/undg_country/themes/custom/undg/images/SDGs/pt-br/SDG-7.svg"></img></td>
-                    <td>Energia Acessível e Limpa</td>
-                    <td>Este objetivo busca garantir o acesso universal a serviços energéticos confiáveis, sustentáveis e modernos, promovendo o uso de fontes de energia renovável e eficiência energética.</td>
+                    <td><h2>Energia Acessível e Limpa</h2></td>
+                    <td><p>Este objetivo busca garantir o acesso universal a serviços energéticos confiáveis, sustentáveis e modernos, promovendo o uso de fontes de energia renovável e eficiência energética.</p></td>
                 </tr>
                 <tr>
                     <td><img src="https://brasil.un.org/profiles/undg_country/themes/custom/undg/images/SDGs/pt-br/SDG-8.svg"></img></td>
-                    <td>Trabalho Decente e Crescimento Econômico</td>
-                    <td>O objetivo é promover o crescimento econômico sustentado, inclusivo e sustentável, alcançando o pleno emprego e trabalho decente para todos, além de promover a produtividade e inovação.</td>
+                    <td><h2>Trabalho Decente e Crescimento Econômico</h2></td>
+                    <td><p>O objetivo é promover o crescimento econômico sustentado, inclusivo e sustentável, alcançando o pleno emprego e trabalho decente para todos, além de promover a produtividade e inovação.</p></td>
                 </tr>
                 <tr>
                     <td><img src="https://brasil.un.org/profiles/undg_country/themes/custom/undg/images/SDGs/pt-br/SDG-9.svg"></img></td>
-                    <td>Indústria, Inovação e Infraestrutura</td>
-                    <td>Este objetivo busca construir infraestrutura resiliente, promover a industrialização inclusiva e sustentável, e fomentar a inovação para promover o desenvolvimento econômico e social.</td>
+                    <td><h2>Indústria, Inovação e Infraestrutura</h2></td>
+                    <td><p>Este objetivo busca construir infraestrutura resiliente, promover a industrialização inclusiva e sustentável, e fomentar a inovação para promover o desenvolvimento econômico e social.</p></td>
                 </tr>
                 <tr>
                     <td><img src="https://brasil.un.org/profiles/undg_country/themes/custom/undg/images/SDGs/pt-br/SDG-10.svg"></img></td>
-                    <td>Redução das Desigualdades</td>
-                    <td>O objetivo é reduzir as desigualdades dentro e entre os países, promovendo políticas e medidas que abordem as disparidades econômicas, sociais e políticas.</td>
+                    <td><h2>Redução das Desigualdades</h2></td>
+                    <td><p>O objetivo é reduzir as desigualdades dentro e entre os países, promovendo políticas e medidas que abordem as disparidades econômicas, sociais e políticas.</p></td>
                 </tr>
                 <tr>
                     <td><img src="https://brasil.un.org/profiles/undg_country/themes/custom/undg/images/SDGs/pt-br/SDG-11.svg"></img></td>
-                    <td>Cidades e Comunidades Sustentáveis</td>
-                    <td>Este objetivo visa tornar as cidades e os assentamentos humanos inclusivos, seguros, resilientes e sustentáveis, promovendo o planejamento urbano integrado e o acesso a infraestrutura básica.</td>
+                    <td><h2>Cidades e Comunidades Sustentáveis</h2></td>
+                    <td><p>Este objetivo visa tornar as cidades e os assentamentos humanos inclusivos, seguros, resilientes e sustentáveis, promovendo o planejamento urbano integrado e o acesso a infraestrutura básica.</p></td>
                 </tr>
                 <tr>
                     <td><img src="https://brasil.un.org/profiles/undg_country/themes/custom/undg/images/SDGs/pt-br/SDG-12.svg"></img></td>
-                    <td>Consumo e Produção Sustentáveis</td>
-                    <td>O objetivo é garantir padrões sustentáveis de produção e consumo, promovendo o uso eficiente de recursos naturais, a redução do desperdício e a adoção de práticas de consumo responsável.</td>
+                    <td><h2>Consumo e Produção Sustentáveis</h2></td>
+                    <td><p>O objetivo é garantir padrões sustentáveis de produção e consumo, promovendo o uso eficiente de recursos naturais, a redução do desperdício e a adoção de práticas de consumo responsável.</p></td>
                 </tr>
             </table>
         </div>
